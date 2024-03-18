2db3fd8fbfd223b1616b3570f2be46bb447f5b79
jsenn <jsenn1995@gmail.com>
2024-03-17 22:49:42 -0400

    add logging, try again

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import datetime
-import time
 from concurrent.futures import ThreadPoolExecutor
+
+import logging
+import time
 
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=f'logs/nlrb_case_scraper_{datetime.datetime}.log', level=logging.INFO)
 
 url = 'https://www.nlrb.gov/case/'
 data_dir = 'case_htmls'
 
 
 def timing(func):
+    """
+    Decorator. Time the execution of a function.
+    :param func: function to time
+    :return:
+    """
     def wrapper(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
@@ -20,29 +30,50 @@ def timing(func):
     return wrapper
 
 
+def try_again(func):
+    """
+    Decorator. Retry a function if it fails.
+    :param func: function to retry
+    :return:
+    """
+    def wrapper(*args, **kwargs):
+        max_attempts = 3
+        delay = 3
+        for i in range(max_attempts):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                time.sleep(delay)
+        logger.error(f'Error executing {func.__name__} with {",".join(*args)} and {", ".join(**kwargs)}: {e}')
+
+    return wrapper
+
+
 def load_cases():
     cases = pd.read_csv('cases.csv')
     return cases['Case Number'].tolist()
 
 
+@try_again
 def scrape_case(case):
     case_url = url + case
     response = requests.get(case_url)
     if response.status_code != 200:
-        print(f'Error: {response.status_code} for {case_url}')
-        return
+        logger.warning(f'Error: {response.status_code} for {case_url}')
+        raise Exception(f'Error: {response.status_code} for {case_url}')
+
     case_soup = bs(response.content, 'html.parser').prettify()
 
     # TODO: do we care about diffs?
     with open(f'{data_dir}/{case}.html', 'w', encoding='utf-8') as f:
         f.write(case_soup)
-        print(f'Downloaded {case}.html at {time.localtime()}')
+        logger.debug(f'Downloaded {case}.html at {time.localtime()}')
     return
 
 
 @timing
 def parallel_scrape(cases):
-    with ThreadPoolExecutor(max_workers=20) as executor:
+    with ThreadPoolExecutor(max_workers=50) as executor:
         results = executor.map(scrape_case, cases)
     return results
 
@@ -50,10 +81,10 @@ def parallel_scrape(cases):
 @timing
 def main():
     case_numbers = load_cases()
-    print(f"Downloading {len(case_numbers)} cases")
+    logger.info(f"Downloading {len(case_numbers)} cases")
 
     parallel_scrape(case_numbers)
-    print(f'Downloaded {len(case_numbers)} at {datetime.datetime.now()}')
+    logger.info(f'Downloaded {len(case_numbers)} at {datetime.datetime.now()}')
     return
 
 
